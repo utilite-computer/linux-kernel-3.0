e6b9e55d491dd525cee4d2580d59501239565afe
Dmitry Lifshitz <lifshitz@compulab.co.il>
2013-07-17 11:08:33 +0300

    Input: himax_ts: add support for more models
    Add support for the hx8526 controller variant.
    
    Signed-off-by: Dmitry Lifshitz <lifshitz@compulab.co.il>
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- drivers/input/touchscreen/himax_ts.c
+++ drivers/input/touchscreen/himax_ts.c
@@ -1,5 +1,6 @@
 /*
- * Touch Screen driver for DataImage's I2C connected touch screen panels
+ * Touch Screen driver for Himax touchscreen controllers used in
+ * DataImage's I2C connected touchscreen panels.
  *   Copyright (c) 2012 Anders Electronics
  *   Copyright 2012 CompuLab Ltd, Dmitry Lifshitz <lifshitz@compulab.co.il>
  *
@@ -31,133 +32,327 @@
 #include <linux/io.h>
 #include <linux/i2c.h>
 #include <linux/timer.h>
+#include <linux/input/mt.h>
 
-#define MAX_X 480
-#define MAX_Y 800
+#define HX_MAX_X		480
+#define HX_MAX_Y		800
 
-#define FINGERS 2
-#define FINGER_SIZE 4
+#define HX_PNT_SIZE		4
+#define HX_EMPTY		0xFFFF
 
-struct di_ts_priv {
-	struct i2c_client *client;
-	struct input_dev *input[FINGERS];
-	char phys[FINGERS][32];
-	char name[FINGERS][32];
-	int irq;
-	int opened;
+struct himax_ts_initseq_entry {
+	char *cmd;
+	int count;
+	int delay_ms;
 };
 
-static irqreturn_t di_ts_isr(int irq, void *dev_id)
+struct himax_ts_props_entry {
+	int model;
+	struct himax_ts_initseq_entry *initseq;
+	int initseq_size;
+	int packet_size;
+	int touch_points;
+	bool invert_x;
+	bool invert_y;
+	bool xy_order;
+};
+
+struct himax_ts_priv {
+	struct i2c_client *client;
+	struct himax_ts_props_entry *ts_props;
+	struct input_dev *input;
+	int prev_touches;
+	int irq;
+	char *buf;
+};
+
+static char hx85x_ic_poweron_cmd[]	= {0x81};
+static char hx85x_mcu_poweron_cmd[]	= {0x35, 0x02};
+static char hx85x_senseon_cmd[]		= {0x83};
+static char hx85x_senseoff_cmd[]	= {0x82};
+static char hx85x_get_id_cmd[]		= {0x31};
+static char hx85x_get_event_cmd[]	= {0x85};
+static char hx85x_get_sleep_cmd[]	= {0x63};
+
+static char hx8526_flash_poweron_cmd[]	= {0x36, 0x0F, 0x53};
+static char hx8526_fetch_flash_cmd[]	= {0xDD, 0x04, 0x02};
+
+static char hx8520_flash_poweron_cmd[]	= {0x36, 0x01};
+static char hx8520_speed_mode_cmd[]	= {0x9D, 0x80};
+
+static struct himax_ts_initseq_entry hx8526_initseq[] = {
+	{hx85x_ic_poweron_cmd,     ARRAY_SIZE(hx85x_ic_poweron_cmd), 120},
+	{hx85x_mcu_poweron_cmd,    ARRAY_SIZE(hx85x_mcu_poweron_cmd), 10},
+	{hx8526_flash_poweron_cmd, ARRAY_SIZE(hx8526_flash_poweron_cmd), 10},
+	{hx8526_fetch_flash_cmd,   ARRAY_SIZE(hx8526_fetch_flash_cmd), 10},
+};
+
+static struct himax_ts_initseq_entry hx8520_initseq[] = {
+	{hx85x_ic_poweron_cmd,     ARRAY_SIZE(hx85x_ic_poweron_cmd), 120},
+	{hx8520_speed_mode_cmd,    ARRAY_SIZE(hx8520_speed_mode_cmd), 10},
+	{hx85x_mcu_poweron_cmd,    ARRAY_SIZE(hx85x_mcu_poweron_cmd), 10},
+	{hx8520_flash_poweron_cmd, ARRAY_SIZE(hx8520_flash_poweron_cmd), 10},
+};
+
+static struct himax_ts_props_entry himax_ts_props[] = {
+	{
+		.model		= 0x8520,
+		.initseq	= hx8520_initseq,
+		.initseq_size	= ARRAY_SIZE(hx8520_initseq),
+		.packet_size	= 16,
+		.touch_points	= 2,
+		.invert_y	= true,
+	},
+	{
+		.model		= 0x8526,
+		.initseq	= hx8526_initseq,
+		.initseq_size	= ARRAY_SIZE(hx8526_initseq),
+		.packet_size	= 32,
+		.touch_points	= 5,
+		.xy_order	= true,
+	},
+};
+
+static void himax_ts_set_coords(struct himax_ts_props_entry *props,
+				u32 *px, u32 *py)
 {
-	struct di_ts_priv *priv = dev_id;
-	const u8 NO_EVENT = 0xFF;
-	unsigned short i = 0, xpos, ypos;
-	u8 getEvent = 0x85;
-	u8 buf[16];
+	u32 x = *px;
+	u32 y = *py;
 
-	memset(buf, 0, sizeof(buf));
+	if (!props->xy_order)
+		swap(x, y);
 
-	if (i2c_master_send(priv->client, &getEvent, 1) != 1) {
-		dev_err(&priv->client->dev, "Unable to write i2c\n");
+	if (props->invert_x && x != HX_EMPTY)
+		x = HX_MAX_X - x;
+
+	if (props->invert_y && y != HX_EMPTY)
+		y = HX_MAX_Y - y;
+
+	*px = x;
+	*py = y;
+}
+
+static irqreturn_t himax_ts_isr(int irq, void *data)
+{
+	struct himax_ts_priv *priv = data;
+	struct himax_ts_props_entry *props = priv->ts_props;
+	struct input_dev *input = priv->input;
+	int packet_size = props->packet_size;
+	char *buf = priv->buf;
+	int curr_touches, touch_count, i;
+	u32 x, y;
+	bool was_touched, now_touched, report_event;
+
+	memset(buf, 0, packet_size);
+
+	if (i2c_master_send(priv->client, hx85x_get_event_cmd, 1) != 1) {
+		dev_err(&priv->client->dev, "Unable to write get event cmd\n");
 		return IRQ_HANDLED;
 	}
 
-	/* Now do Page Read */
-	if (i2c_master_recv(priv->client, buf, sizeof(buf)) != sizeof(buf)) {
-		dev_err(&priv->client->dev, "Unable to read data from i2c\n");
+	if (i2c_master_recv(priv->client, buf, packet_size) != packet_size) {
+		dev_err(&priv->client->dev, "Unable to read events data\n");
 		return IRQ_HANDLED;
 	}
 
-	for (i = 0; i < FINGERS; i++) {
-		dev_dbg(&priv->client->dev,
-			"Buffer [%d]: %02X %02X %02X %02X\n",
-			i, buf[i * FINGER_SIZE], buf[i * FINGER_SIZE + 1],
-			buf[i * FINGER_SIZE + 2], buf[i * FINGER_SIZE + 3]);
+	/*
+	 * Two last bytes in the buffer correspond to invalid data. Next two
+	 * from the end, correspond to touch counter and touch points ids.
+	 */
 
-		if (buf[i * FINGER_SIZE] == NO_EVENT) {
-			dev_dbg(&priv->client->dev, "Untouched");
+	/* Retrieve touch points counter. 0x0F corresponds to 0 touches */
+	touch_count = buf[packet_size - 2 - 2] & 0x0F;
+	if (touch_count == 0x0F)
+		touch_count = 0;
 
-			input_report_key(priv->input[i], BTN_TOUCH, 0);
-			input_report_abs(priv->input[i], ABS_PRESSURE, 0);
-			input_sync(priv->input[i]);
-		} else {
-			ypos = MAX_Y - (buf[i * FINGER_SIZE + 1] |
-					(buf[i * FINGER_SIZE + 0] << 8));
+	/* According to the Himax code examples, this value can be invalid */
+	if (touch_count > props->touch_points)
+		return IRQ_HANDLED;
 
-			xpos = buf[i * FINGER_SIZE + 3] |
-				(buf[i * FINGER_SIZE + 2] << 8);
+	/* Retrieve touch points ids. 0xFF corresponds to 0 touches */
+	curr_touches = buf[packet_size - 2 - 1];
+	if (curr_touches == 0xFF)
+		curr_touches = 0;
 
-			dev_dbg(&priv->client->dev, "Touched: x = %d, y = %d\n",
-				xpos, ypos);
+	for (i = 0; i < props->touch_points; i++) {
+		x = (buf[i * HX_PNT_SIZE + 0] << 8) | buf[i * HX_PNT_SIZE + 1];
+		y = (buf[i * HX_PNT_SIZE + 2] << 8) | buf[i * HX_PNT_SIZE + 3];
 
-			input_report_key(priv->input[i], BTN_TOUCH, 1);
-			input_report_abs(priv->input[i], ABS_PRESSURE, 0xFF);
-			input_report_abs(priv->input[i], ABS_X, xpos);
-			input_report_abs(priv->input[i], ABS_Y, ypos);
-			input_sync(priv->input[i]);
+		himax_ts_set_coords(props, &x, &y);
+
+		report_event = false;
+		was_touched = priv->prev_touches & (1 << i);
+		now_touched = curr_touches & (1 << i);
+
+		/* Check for touch state and coordinates consistency */
+		if (now_touched && (x <= HX_MAX_X && y <= HX_MAX_Y)) {
+			report_event = true;
+			dev_dbg(&priv->client->dev, "# %d x=%d y=%d", i, x, y);
+		} else if (was_touched && (x == HX_EMPTY && y == HX_EMPTY)) {
+			report_event = true;
+			dev_dbg(&priv->client->dev, "# %d released", i);
+		}
+
+		if (report_event) {
+			input_mt_slot(priv->input, i);
+			input_mt_report_slot_state(input, MT_TOOL_FINGER,
+							now_touched);
+			if (now_touched) {
+				input_report_abs(input, ABS_MT_POSITION_X, x);
+				input_report_abs(input, ABS_MT_POSITION_Y, y);
+				input_report_abs(input, ABS_MT_PRESSURE, 0xFF);
+			}
 		}
 	}
+
+	input_mt_report_pointer_emulation(input, true);
+	input_sync(input);
+
+	priv->prev_touches = curr_touches;
 
 	return IRQ_HANDLED;
 }
 
-static int di_ts_open(struct input_dev *dev)
+static int himax_ts_setup(struct himax_ts_priv *priv)
 {
-	struct di_ts_priv *priv = input_get_drvdata(dev);
+	struct himax_ts_props_entry *props = priv->ts_props;
 	struct i2c_client *client = priv->client;
-	char sleepOut = 0x81;
-	char senseOn = 0x83;
-	char clearStack = 0x88;
-	char speedMode[] = { 0x9D, 0x80 };
-	char mcuTurnOn[] = { 0x35, 0x02 };
-	char flashTurnOn[] = { 0x36, 0x01 };
+	char *cmd = hx85x_get_sleep_cmd;
+	char buf = 0x00;
+	int count, i;
 
-	if (priv->opened)
+	if (i2c_master_send(client, cmd, 1) != 1)
+		goto err_stop_seq;
+
+	if (i2c_master_recv(client, &buf, 1) != 1) {
+		dev_err(&client->dev, "Failed to read get sleep data\n");
+		return -EBUSY;
+	}
+
+	if (buf != 0) {
+		dev_dbg(&client->dev, "already initialized 0x%02X\n", buf);
 		return 0;
+	}
 
-	priv->opened = 1;
+	for (i = 0; i < props->initseq_size; i++) {
+		cmd = props->initseq[i].cmd;
+		count = props->initseq[i].count;
 
-	if (i2c_master_send(client, &sleepOut, 1) != 1)
-		dev_err(&client->dev, "Unable to sleep out\n");
+		if (i2c_master_send(client, cmd, count) != count)
+			goto err_stop_seq;
 
-	msleep(120);
+		msleep(props->initseq[i].delay_ms);
+	}
 
-	if (i2c_master_send(client, speedMode, 2) != 2)
-		dev_err(&client->dev, "Unable to set speed mode\n");
+	return 0;
 
-	if (i2c_master_send(client, mcuTurnOn, 2) != 2)
-		dev_err(&client->dev, "Unable to turn on the MCU\n");
+err_stop_seq:
+	dev_err(&client->dev, "Failed to send I2C command 0x%02X\n", cmd[0]);
+	return -EBUSY;
+}
 
-	if (i2c_master_send(client, flashTurnOn, 2) != 2)
-		dev_err(&client->dev, "Unable to turn on the flash\n");
+static int himax_ts_open(struct input_dev *dev)
+{
+	struct himax_ts_priv *priv = input_get_drvdata(dev);
+	struct i2c_client *client = priv->client;
 
-	if (i2c_master_send(client, &senseOn, 1) != 1)
-		dev_err(&client->dev, "Unable to set sense on\n");
+	if (i2c_master_send(client, hx85x_senseon_cmd, 1) != 1) {
+		dev_err(&priv->client->dev, "failed to write sense on command\n");
+		return -EBUSY;
+	}
 
 	msleep(100);
-
-	if (i2c_master_send(client, &clearStack, 1) != 1)
-		dev_err(&client->dev, "Unable to clear stack\n");
 
 	return 0;
 }
 
-static int di_ts_probe(struct i2c_client *client,
+static void himax_ts_close(struct input_dev *dev)
+{
+	struct himax_ts_priv *priv = input_get_drvdata(dev);
+	struct i2c_client *client = priv->client;
+
+	if (i2c_master_send(client, hx85x_senseoff_cmd, 1) != 1)
+		dev_err(&priv->client->dev, "failed to write sense off command\n");
+}
+
+static struct input_dev *himax_ts_init_input(struct himax_ts_priv *priv)
+{
+	struct input_dev *input;
+
+	input = input_allocate_device();
+	if (!input) {
+		dev_err(&priv->client->dev, "Failed to allocate input device\n");
+		return NULL;
+	}
+
+	input->name = priv->client->name;
+	input->phys = priv->client->adapter->name,
+	input->id.bustype = BUS_I2C;
+	input->dev.parent = &priv->client->dev;
+	input->open = himax_ts_open;
+	input->close = himax_ts_close;
+
+	__set_bit(EV_KEY, input->evbit);
+	__set_bit(EV_ABS, input->evbit);
+	__set_bit(EV_SYN, input->evbit);
+	__set_bit(BTN_TOUCH, input->keybit);
+	__set_bit(ABS_X, input->absbit);
+	__set_bit(ABS_Y, input->absbit);
+
+	input_set_abs_params(input, ABS_X, 0, HX_MAX_X, 0, 0);
+	input_set_abs_params(input, ABS_Y, 0, HX_MAX_Y, 0, 0);
+	input_set_abs_params(input, ABS_PRESSURE, 0, 0xFF, 0, 0);
+
+	input_set_abs_params(input, ABS_MT_POSITION_X, 0, HX_MAX_X, 0, 0);
+	input_set_abs_params(input, ABS_MT_POSITION_Y, 0, HX_MAX_Y, 0, 0);
+	input_set_abs_params(input, ABS_MT_PRESSURE, 0, 0xff, 0, 0);
+
+	input_mt_init_slots(input, priv->ts_props->touch_points);
+
+	input_set_drvdata(input, priv);
+
+	return input;
+}
+
+static int himax_ts_probe(struct i2c_client *client,
 			const struct i2c_device_id *idp)
 {
-	struct di_ts_priv *priv;
-	struct input_dev *input[FINGERS];
-	int error, i = 0;
+	struct himax_ts_priv *priv;
+	struct himax_ts_props_entry *ts_props = NULL;
+	int error, i;
 	char buf[3];
-	char getDevId = 0x31;
+	int chip_model;
 
-	if (i2c_master_send(client, &getDevId, 1) == 1 &&
-		i2c_master_recv(client, buf, sizeof(buf)) == sizeof(buf)) {
-		dev_info(&client->dev, "Device ID: 0x%02X%02X%02X\n",
+	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
+		dev_err(&client->dev, "I2C_FUNC_I2C check failed\n");
+		return -EBUSY;
+	}
+
+	if (i2c_master_send(client, hx85x_get_id_cmd, 1) == 1 &&
+		i2c_master_recv(client, buf, 3) == 3) {
+		dev_info(&client->dev, "Found device ID: 0x%02X%02X%02X\n",
 			 buf[0], buf[1], buf[2]);
 	} else {
 		dev_err(&client->dev, "Unable to get DevId\n");
 		return -ENODEV;
+	}
+
+	chip_model = (buf[0] << 8) | buf[1];
+
+	for (i = 0; i < ARRAY_SIZE(himax_ts_props); i++) {
+		if (chip_model == himax_ts_props[i].model) {
+			ts_props = &himax_ts_props[i];
+			break;
+		}
+	}
+
+	if (!ts_props) {
+		dev_err(&client->dev, "Unsupported device model\n");
+		return -ENODEV;
+	} else if (ts_props->model != idp->driver_data) {
+		dev_warn(&client->dev,
+		"Requested model 0x%04X not found, proceed with 0x%04X setup\n",
+			(unsigned int)idp->driver_data, ts_props->model);
 	}
 
 	priv = kzalloc(sizeof(*priv), GFP_KERNEL);
@@ -168,54 +363,37 @@ static int di_ts_probe(struct i2c_client *client,
 
 	priv->client = client;
 	priv->irq = client->irq;
+	priv->ts_props = ts_props;
 
-	for (i = 0; i < FINGERS; i++) {
-		input[i] = input_allocate_device();
-		if (!input[i]) {
-			dev_err(&client->dev,
-				"Failed to allocate input device.\n");
-			error = -ENOMEM;
-			goto err_free_mem;
-		}
+	error = himax_ts_setup(priv);
+	if (error)
+		goto err_free_mem;
 
-		priv->input[i] = input[i];
-
-		__set_bit(EV_KEY, input[i]->evbit);
-		__set_bit(EV_ABS, input[i]->evbit);
-		__set_bit(EV_SYN, input[i]->evbit);
-		__set_bit(BTN_TOUCH, input[i]->keybit);
-
-		input_set_abs_params(input[i], ABS_X, 0, MAX_X, 0, 0);
-		input_set_abs_params(input[i], ABS_Y, 0, MAX_Y, 0, 0);
-		input_set_abs_params(input[i], ABS_PRESSURE, 0, 0xff, 0, 0);
-
-		snprintf(priv->phys[i], sizeof(priv->phys[i]),
-				"%s/input%d", client->name, i);
-		snprintf(priv->name[i], sizeof(priv->name[i]),
-				"%s%d", client->name, i);
-
-		input[i]->phys = priv->phys[i];
-		input[i]->name = priv->name[i];
-		input[i]->id.bustype = BUS_I2C;
-		input[i]->dev.parent = &client->dev;
-
-		input[i]->open = di_ts_open;
-
-		input_set_drvdata(input[i], priv);
+	priv->buf = kzalloc(priv->ts_props->packet_size, GFP_KERNEL);
+	if (!priv->buf) {
+		dev_err(&client->dev, "failed to allocate read buffer\n");
+		error = -ENOMEM;
+		goto err_free_mem;
 	}
 
-	error = request_threaded_irq(priv->irq, NULL, di_ts_isr,
+	priv->input = himax_ts_init_input(priv);
+	if (!priv->input) {
+		error = -ENOMEM;
+		goto err_free_mem;
+	}
+
+	error = input_register_device(priv->input);
+	if (error) {
+		dev_err(&client->dev, "Failed to register input device.\n");
+		goto err_free_mem;
+	}
+
+	error = request_threaded_irq(priv->irq, NULL, himax_ts_isr,
 					IRQF_TRIGGER_LOW | IRQF_ONESHOT,
 					client->name, priv);
 	if (error) {
 		dev_err(&client->dev, "Unable to request touchscreen IRQ.\n");
-		goto err_free_mem;
-	}
-
-	for (i = 0; i < FINGERS; i++) {
-		error = input_register_device(input[i]);
-		if (error)
-			goto err_free_irq;
+		goto err_free_dev;
 	}
 
 	i2c_set_clientdata(client, priv);
@@ -223,37 +401,34 @@ static int di_ts_probe(struct i2c_client *client,
 
 	return 0;
 
-err_free_irq:
-	free_irq(priv->irq, priv);
+err_free_dev:
+	input_unregister_device(priv->input);
+	priv->input = NULL;
 err_free_mem:
-	for ( ; i >= 0; i--)
-		input_free_device(input[i]);
-
-	i2c_set_clientdata(client, NULL);
+	input_free_device(priv->input);
+	kfree(priv->buf);
 	kfree(priv);
 
 	return error;
 }
 
-static int di_ts_remove(struct i2c_client *client)
+static int himax_ts_remove(struct i2c_client *client)
 {
-	struct di_ts_priv *priv = i2c_get_clientdata(client);
-	int i;
+	struct himax_ts_priv *priv = i2c_get_clientdata(client);
 
 	free_irq(priv->irq, priv);
-	for (i = FINGERS; i >= 0; i--)
-		input_unregister_device(priv->input[i]);
-
 	i2c_set_clientdata(client, NULL);
+	input_unregister_device(priv->input);
+	kfree(priv->buf);
 	kfree(priv);
 
 	return 0;
 }
 
-static int di_ts_suspend(struct device *dev)
+static int himax_ts_suspend(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
-	struct di_ts_priv *priv = i2c_get_clientdata(client);
+	struct himax_ts_priv *priv = i2c_get_clientdata(client);
 
 	if (device_may_wakeup(&client->dev))
 		enable_irq_wake(priv->irq);
@@ -261,10 +436,10 @@ static int di_ts_suspend(struct device *dev)
 	return 0;
 }
 
-static int di_ts_resume(struct device *dev)
+static int himax_ts_resume(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
-	struct di_ts_priv *priv = i2c_get_clientdata(client);
+	struct himax_ts_priv *priv = i2c_get_clientdata(client);
 
 	if (device_may_wakeup(&client->dev))
 		disable_irq_wake(priv->irq);
@@ -272,37 +447,39 @@ static int di_ts_resume(struct device *dev)
 	return 0;
 }
 
-static SIMPLE_DEV_PM_OPS(di_ts_pm, di_ts_suspend, di_ts_resume);
+static SIMPLE_DEV_PM_OPS(himax_ts_pm, himax_ts_suspend, himax_ts_resume);
 
-static const struct i2c_device_id di_ts_id[] = {
-	{ "hx8520-c", 0 },
-	{ }
+static const struct i2c_device_id himax_ts_id[] = {
+	{ "hx8520-c"	, 0x8520 },
+	{ "hx8526-a"	, 0x8526 },
+	{ },
 };
-MODULE_DEVICE_TABLE(i2c, di_ts_id);
 
-static struct i2c_driver di_ts_driver = {
+MODULE_DEVICE_TABLE(i2c, himax_ts_id);
+
+static struct i2c_driver himax_ts_driver = {
 	.driver = {
 		.owner = THIS_MODULE,
-		.name = "hx8520-c",
-		.pm = &di_ts_pm,
+		.name = "himax_ts",
+		.pm = &himax_ts_pm,
 	},
-	.probe = di_ts_probe,
-	.remove = di_ts_remove,
-	.id_table = di_ts_id,
+	.probe = himax_ts_probe,
+	.remove = himax_ts_remove,
+	.id_table = himax_ts_id,
 };
 
-static int __init di_ts_init(void)
+static int __init himax_ts_init(void)
 {
-	return i2c_add_driver(&di_ts_driver);
+	return i2c_add_driver(&himax_ts_driver);
 }
 
-static void __exit di_ts_exit(void)
+static void __exit himax_ts_exit(void)
 {
-	i2c_del_driver(&di_ts_driver);
+	i2c_del_driver(&himax_ts_driver);
 }
 
-module_init(di_ts_init);
-module_exit(di_ts_exit);
+module_init(himax_ts_init);
+module_exit(himax_ts_exit);
 
-MODULE_DESCRIPTION("HX8520-C Touchscreen driver");
+MODULE_DESCRIPTION("Himax Touchscreen driver");
 MODULE_LICENSE("GPL v2");
