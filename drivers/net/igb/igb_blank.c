6e0c5ac72b3ca4614aaabb7db4831ace0cb254f7
Igor Grinberg <grinberg@compulab.co.il>
2013-07-25 11:29:55 +0300

    net: igb: igb_blank: cleanup and fix warnings
    Clean up the driver and fix section mismatch warnings like the one
    below:
    
    WARNING: drivers/net/igb/igb_blank.o(.data+0xc): Section mismatch in
    reference from the variable igb_driver_blank to the (unknown reference)
    .devinit.rodata:(unknown)
    The variable igb_driver_blank references
    the (unknown reference) __devinitconst (unknown)
    If the reference is valid then annotate the
    variable with __init* or __refdata (see linux/init.h) or name the
    variable:
    *driver, *_template, *_timer, *_sht, *_ops, *_probe, *_probe_one,
    *_console
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- drivers/net/igb/igb_blank.c
+++ drivers/net/igb/igb_blank.c
@@ -7,10 +7,7 @@
 
 #include "igb.h"
 
-#define DRV_VERSION "1.0"
-
-char igb_driver_name[] = "igb_blank";
-char igb_driver_version[] = DRV_VERSION;
+#define MODULE_NAME "igb_blank"
 
 static DEFINE_PCI_DEVICE_TABLE(igb_pci_tbl_blank) = {
 	{ PCI_VDEVICE(INTEL, E1000_DEV_ID_I211_BLANK) },
@@ -19,16 +16,13 @@ static DEFINE_PCI_DEVICE_TABLE(igb_pci_tbl_blank) = {
 };
 MODULE_DEVICE_TABLE(pci, igb_pci_tbl_blank);
 
-static int igb_probe_blank(struct pci_dev *, const struct pci_device_id *);
-static void __devexit igb_remove_blank(struct pci_dev *pdev);
-
 static int __devinit igb_probe_blank(struct pci_dev *pdev,
 			       const struct pci_device_id *ent)
 {
 	struct net_device *netdev;
 	struct igb_adapter *adapter;
 	struct e1000_hw *hw;
-	int err, pci_using_dac;
+	int err, pci_using_dac, pci_bars;
 
 	err = pci_enable_device_mem(pdev);
 	if (err)
@@ -37,16 +31,17 @@ static int __devinit igb_probe_blank(struct pci_dev *pdev,
 	pci_using_dac = 0;
 	err = dma_set_mask(pci_dev_to_dev(pdev), DMA_BIT_MASK(64));
 	if (!err) {
-		err = dma_set_coherent_mask(pci_dev_to_dev(pdev), DMA_BIT_MASK(64));
+		err = dma_set_coherent_mask(pci_dev_to_dev(pdev),
+					    DMA_BIT_MASK(64));
 		if (!err)
 			pci_using_dac = 1;
 	} else {
 		err = dma_set_mask(pci_dev_to_dev(pdev), DMA_BIT_MASK(32));
 		if (err) {
-			err = dma_set_coherent_mask(pci_dev_to_dev(pdev), DMA_BIT_MASK(32));
+			err = dma_set_coherent_mask(pci_dev_to_dev(pdev),
+						    DMA_BIT_MASK(32));
 			if (err) {
-				IGB_ERR("No usable DMA configuration, "
-				        "aborting\n");
+				IGB_ERR("No usable DMA configuration, abort\n");
 				goto err_dma;
 			}
 		}
@@ -63,14 +58,11 @@ static int __devinit igb_probe_blank(struct pci_dev *pdev,
 	hw = &adapter->hw;
 	hw->back = adapter;
 
-	err = pci_request_selected_regions(pdev,
-	                                   pci_select_bars(pdev,
-                                                           IORESOURCE_MEM),
-	                                   igb_driver_name);
+	pci_bars = pci_select_bars(pdev, IORESOURCE_MEM);
+	err = pci_request_selected_regions(pdev, pci_bars, MODULE_NAME);
 	if (err)
 		goto err_pci_reg;
 
-	err = -EIO;
 	hw->hw_addr = ioremap(pci_resource_start(pdev, 0),
 	                      pci_resource_len(pdev, 0));
 	if (!hw->hw_addr)
@@ -79,8 +71,9 @@ static int __devinit igb_probe_blank(struct pci_dev *pdev,
 	return 0;
 
 err_ioremap:
-	pci_release_selected_regions(pdev,
-	                             pci_select_bars(pdev, IORESOURCE_MEM));
+	err = -EIO;
+	pci_bars = pci_select_bars(pdev, IORESOURCE_MEM);
+	pci_release_selected_regions(pdev, pci_bars);
 err_pci_reg:
 err_dma:
 	pci_disable_device(pdev);
@@ -94,39 +87,38 @@ static void __devexit igb_remove_blank(struct pci_dev *pdev)
 	struct e1000_hw *hw = &adapter->hw;
 
 	iounmap(hw->hw_addr);
+
 	if (hw->flash_address)
 		iounmap(hw->flash_address);
+
 	pci_release_selected_regions(pdev,
 	                    pci_select_bars(pdev, IORESOURCE_MEM));
 
 	free_netdev(netdev);
-
 	pci_disable_device(pdev);
 }
 
-static struct pci_driver igb_driver_blank = {
-	.name     = igb_driver_name,
+static struct pci_driver igb_blank_driver = {
+	.name     = MODULE_NAME,
 	.id_table = igb_pci_tbl_blank,
 	.probe    = igb_probe_blank,
 	.remove   = __devexit_p(igb_remove_blank),
 };
 
-static int __init igb_init_module_blank(void)
+static int __init igb_blank_init_module(void)
 {
-	int ret;
-	ret = pci_register_driver(&igb_driver_blank);
-	return ret;
+	return pci_register_driver(&igb_blank_driver);
 }
-module_init(igb_init_module_blank);
+module_init(igb_blank_init_module);
 
-static void __exit igb_exit_module_blank(void)
+static void __exit igb_blank_cleanup_module(void)
 {
-	pci_unregister_driver(&igb_driver_blank);
+	pci_unregister_driver(&igb_blank_driver);
 }
-module_exit(igb_exit_module_blank);
+module_exit(igb_blank_cleanup_module);
 
 
 MODULE_AUTHOR("CompuLab");
 MODULE_DESCRIPTION("CompuLab Blank IGB Driver");
 MODULE_LICENSE("GPL");
-MODULE_VERSION(DRV_VERSION);
+MODULE_VERSION("1.0");
