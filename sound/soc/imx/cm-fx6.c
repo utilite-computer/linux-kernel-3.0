9d88046daecad2d41b20cb16bce4f0fddb1aac55
Andrey Gelman <andrey.gelman@compulab.co.il>
2013-11-07 13:53:54 +0200

    ASoC: cm-fx6: clean up the source
    Remove excessive verbosity from the license statement.
    Remove unrequired header files.
    
    Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>
    Signed-off-by: Valentin Raevsky <valentin@compulab.co.il>
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- sound/soc/imx/cm-fx6.c
+++ sound/soc/imx/cm-fx6.c
@@ -10,45 +10,15 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA
- *
  */
 
-#include <linux/module.h>
-#include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <linux/delay.h>
-#include <linux/pm.h>
-#include <linux/bitops.h>
 #include <linux/platform_device.h>
-#include <linux/i2c.h>
-#include <linux/err.h>
-#include <linux/irq.h>
-#include <linux/io.h>
 #include <linux/fsl_devices.h>
-#include <linux/slab.h>
-#include <linux/clk.h>
-#include <sound/core.h>
 #include <sound/pcm.h>
-#include <sound/pcm_params.h>
 #include <sound/soc.h>
+#include <sound/soc-dai.h>
 #include <sound/soc-dapm.h>
-#include <sound/initval.h>
-#include <sound/jack.h>
-#include <mach/dma.h>
-#include <mach/clock.h>
 #include <mach/audmux.h>
-#include <mach/gpio.h>
-#include <asm/mach-types.h>
 
 #include "imx-ssi.h"
 #include "../codecs/wm8731.h"
