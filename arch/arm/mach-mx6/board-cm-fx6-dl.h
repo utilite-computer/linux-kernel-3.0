compulab/cm-fx6-kernel

ARM: i.MX6: cm-fx6: rework the MMC/SD support
Apparently, some SD cards do not work well if the pad settings of the
pads used by the MMC bus are adjusted during the clock change.
Moreover, cm-fx6 does not need any such adjustments and performs fine
with only one time MMC bus pads configuration during the boot process.

Given the above facts, remove the runtime adjustment of the MMC related
pads and use one time configuration during the boot process, thus fixing
the SD cards problem.

While on this, split the pad configuration to half (4 bit) and
full (8 bit) MMC bus width.
Do not configure the pins that are not in use.

Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>
Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>

arch/arm/mach-mx6/board-cm-fx6-dl.h
@@ -169,8 +169,9 @@ static iomux_v3_cfg_t cm_fx6_dl_audmux_pads[] = {
 	MX6DL_PAD_SD2_DAT3__AUDMUX_AUD4_TXC,
 };
 
-#define MX6DL_USDHC_PAD_SETTING(id, speed)	\
-mx6dl_sd##id##_##speed##mhz[] = {		\
+/* 8-bit SDIO bus */
+#define MX6DL_USDHC_PAD_SETTING_FULL(id, speed)	\
+static iomux_v3_cfg_t mx6dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -183,8 +184,9 @@ mx6dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_DAT7__USDHC##id##_DAT7_##speed##MHZ,	\
 }
 
+/* 4-bit SDIO bus */
 #define MX6DL_USDHC_PAD_SETTING_HALF(id, speed)	\
-mx6dl_sd##id##_##speed##mhz[] = {		\
+static iomux_v3_cfg_t mx6dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -193,13 +195,8 @@ mx6dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_DAT3__USDHC##id##_DAT3_##speed##MHZ,	\
 }
 
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(3, 50);
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(3, 100);
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(3, 200);
-
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(1, 50);
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(1, 100);
-static iomux_v3_cfg_t MX6DL_USDHC_PAD_SETTING_HALF(1, 200);
+MX6DL_USDHC_PAD_SETTING_HALF(3, 200);	/* SD3 */
+MX6DL_USDHC_PAD_SETTING_HALF(1, 200);	/* SD1 */
 
 static iomux_v3_cfg_t mx6dl_gpmi_nand[] = {
 	MX6DL_PAD_NANDF_CLE__RAWNAND_CLE,

arch/arm/mach-mx6/board-cm-fx6-q.h
@@ -180,8 +180,9 @@ static iomux_v3_cfg_t cm_fx6_q_audmux_pads[] = {
 	MX6Q_PAD_SD2_DAT3__AUDMUX_AUD4_TXC,
 };
 
-#define MX6Q_USDHC_PAD_SETTING(id, speed)	\
-mx6q_sd##id##_##speed##mhz[] = {		\
+/* 8-bit SDIO bus */
+#define MX6Q_USDHC_PAD_SETTING_FULL(id, speed)	\
+static iomux_v3_cfg_t mx6q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -194,8 +195,9 @@ mx6q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_DAT7__USDHC##id##_DAT7_##speed##MHZ,	\
 }
 
+/* 4-bit SDIO bus */
 #define MX6Q_USDHC_PAD_SETTING_HALF(id, speed)	\
-mx6q_sd##id##_##speed##mhz[] = {		\
+static iomux_v3_cfg_t mx6q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -204,15 +206,8 @@ mx6q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_DAT3__USDHC##id##_DAT3_##speed##MHZ,	\
 }
 
-/* SD3 */
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(3, 50);
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(3, 100);
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(3, 200);
-
-/* SD1 */
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(1, 50);
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(1, 100);
-static iomux_v3_cfg_t MX6Q_USDHC_PAD_SETTING_HALF(1, 200);
+MX6Q_USDHC_PAD_SETTING_HALF(3, 200);	/* SD3 */
+MX6Q_USDHC_PAD_SETTING_HALF(1, 200);	/* SD1 */
 
 static iomux_v3_cfg_t mx6q_gpmi_nand[] = {
 	MX6Q_PAD_NANDF_CLE__RAWNAND_CLE,

utilite-computer/linux-kernel-3.0

arch/arm/mach-mx6/board-cm-fx6-dl.h
@@ -173,8 +173,8 @@ static iomux_v3_cfg_t cm_fx6_dl_pads[] = {
 	MX6DL_PAD_GPIO_19__SPDIF_OUT1,
 };
 
-#define CM_FX6_DL_USDHC_PAD_SETTING(id, speed)	\
-cm_fx6_dl_sd##id##_##speed##mhz[] = {		\
+#define CM_FX6_DL_USDHC_PAD_SETTING_FULL(id, speed)		\
+static iomux_v3_cfg_t cm_fx6_dl_sd##id##_full_##speed##mhz[] = {	\
 	MX6DL_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -187,8 +187,8 @@ cm_fx6_dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_DAT7__USDHC##id##_DAT7_##speed##MHZ,	\
 }
 
-#define CM_FX6_DL_USDHC_PAD_SETTING_HALF(id, speed)	\
-cm_fx6_dl_sd##id##_##speed##mhz[] = {		\
+#define CM_FX6_DL_USDHC_PAD_SETTING_HALF(id, speed)		\
+static iomux_v3_cfg_t cm_fx6_dl_sd##id##_half_##speed##mhz[] = {\
 	MX6DL_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6DL_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -197,13 +197,9 @@ cm_fx6_dl_sd##id##_##speed##mhz[] = {		\
 	MX6DL_PAD_SD##id##_DAT3__USDHC##id##_DAT3_##speed##MHZ,	\
 }
 
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING(3, 50);
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING(3, 100);
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING(3, 200);
-
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING_HALF(1, 50);
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING_HALF(1, 100);
-static iomux_v3_cfg_t CM_FX6_DL_USDHC_PAD_SETTING_HALF(1, 200);
+CM_FX6_DL_USDHC_PAD_SETTING_FULL(3, 200);
+CM_FX6_DL_USDHC_PAD_SETTING_HALF(3, 200);
+CM_FX6_DL_USDHC_PAD_SETTING_HALF(1, 200);
 
 #if defined(CONFIG_MTD_NAND_GPMI_NAND)
 static iomux_v3_cfg_t cm_fx6_dl_gpmi_nand[] = {

arch/arm/mach-mx6/board-cm-fx6-q.h
@@ -183,8 +183,8 @@ static iomux_v3_cfg_t cm_fx6_q_common_pads[] = {
 	MX6Q_PAD_GPIO_19__SPDIF_OUT1,
 };
 
-#define CM_FX6_Q_USDHC_PAD_SETTING(id, speed)	\
-cm_fx6_q_sd##id##_##speed##mhz[] = {		\
+#define CM_FX6_Q_USDHC_PAD_SETTING_FULL(id, speed)		\
+static iomux_v3_cfg_t cm_fx6_q_sd##id##_full_##speed##mhz[] = {	\
 	MX6Q_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -197,8 +197,8 @@ cm_fx6_q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_DAT7__USDHC##id##_DAT7_##speed##MHZ,	\
 }
 
-#define CM_FX6_Q_USDHC_PAD_SETTING_HALF(id, speed)	\
-cm_fx6_q_sd##id##_##speed##mhz[] = {		\
+#define CM_FX6_Q_USDHC_PAD_SETTING_HALF(id, speed)		\
+static iomux_v3_cfg_t cm_fx6_q_sd##id##_half_##speed##mhz[] = {	\
 	MX6Q_PAD_SD##id##_CLK__USDHC##id##_CLK_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_CMD__USDHC##id##_CMD_##speed##MHZ,	\
 	MX6Q_PAD_SD##id##_DAT0__USDHC##id##_DAT0_##speed##MHZ,	\
@@ -207,13 +207,9 @@ cm_fx6_q_sd##id##_##speed##mhz[] = {		\
 	MX6Q_PAD_SD##id##_DAT3__USDHC##id##_DAT3_##speed##MHZ,	\
 }
 
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING(3, 50);
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING(3, 100);
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING(3, 200);
-
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING_HALF(1, 50);
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING_HALF(1, 100);
-static iomux_v3_cfg_t CM_FX6_Q_USDHC_PAD_SETTING_HALF(1, 200);
+CM_FX6_Q_USDHC_PAD_SETTING_FULL(3, 200);
+CM_FX6_Q_USDHC_PAD_SETTING_HALF(3, 200);
+CM_FX6_Q_USDHC_PAD_SETTING_HALF(1, 200);
 
 #if defined(CONFIG_MTD_NAND_GPMI_NAND)
 static iomux_v3_cfg_t cm_fx6_q_gpmi_nand[] = {