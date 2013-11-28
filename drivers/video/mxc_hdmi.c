3837174b2215e8ff77d2214d8de7c57becccf3c7
Igor Grinberg <grinberg@compulab.co.il>
2013-11-28 11:35:21 +0200

    Revert "video: mxc_hdmi: loosen the requirement for CEA"
    This reverts commit 8233583c5ac534e88e4c8c64fb43491e85c65089.
    The commit in subject introduces a regression on some displays and
    therefore should be reverted.
    
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- drivers/video/mxc_hdmi.c
+++ drivers/video/mxc_hdmi.c
@@ -1595,7 +1595,8 @@ static void mxc_hdmi_edid_rebuild_modelist(struct mxc_hdmi *hdmi)
 		 */
 		mode = &hdmi->fbi->monspecs.modedb[i];
 
-		if (!(mode->vmode & FB_VMODE_INTERLACED)) {
+		if (!(mode->vmode & FB_VMODE_INTERLACED) &&
+				(mxc_edid_mode_to_vic(mode) != 0)) {
 
 			dev_dbg(&hdmi->pdev->dev, "Added mode %d:", i);
 			dev_dbg(&hdmi->pdev->dev,
