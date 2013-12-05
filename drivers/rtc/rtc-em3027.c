93bdd721247cb4361d0b7a1a61f0239e340b5e71
Andrey Gelman <andrey.gelman@compulab.co.il>
2013-12-05 19:47:11 +0200

    rtc: em3027: enable alarm functionality
    Enable basic alarm functionality.
    
    Signed-off-by: Andrey Gelman <andrey.gelman@compulab.co.il>
    Signed-off-by: Igor Grinberg <grinberg@compulab.co.il>

--- drivers/rtc/rtc-em3027.c
+++ drivers/rtc/rtc-em3027.c
@@ -41,6 +41,23 @@
 
 #define EM3027_REG_EEPROM_CTRL		0x30
 
+/* Bits in registers */
+#define EM3027_BIT_ON_OFF_CLK_nINT	0x80
+#define EM3027_BIT_ON_OFF_TD1		0x40
+#define EM3027_BIT_ON_OFF_TD0		0x20
+#define EM3027_BIT_ON_OFF_SR		0x10
+#define EM3027_BIT_ON_OFF_EE_REFRESH	0x08
+#define EM3027_BIT_ON_OFF_TMR_AUTOREL	0x04
+#define EM3027_BIT_ON_OFF_TMR_ENABLE	0x02
+#define EM3027_BIT_ON_OFF_WATCH_1HZ	0x01
+
+#define EM3027_BIT_IRQ_SR		0x10
+#define EM3027_BIT_IRQ_VLOW2		0x08
+#define EM3027_BIT_IRQ_VLOW1		0x04
+#define EM3027_BIT_IRQ_TIMER		0x02
+#define EM3027_BIT_IRQ_ALARM		0x01
+
+#define EM3027_IRQ_MASK			0x1f
 #define EM3027_TRICKLE_CHARGER_MASK	0xf0
 
 static struct i2c_driver em3027_driver;
@@ -92,6 +109,28 @@ static int em3027_write_register(struct i2c_client *client,
 	}
 
 	return 0;
+}
+
+/**
+ * Update particular bits in a register
+ */
+static int em3027_update_register(struct i2c_client *client,
+				  unsigned char addr,
+				  unsigned char mask,
+				  unsigned char value)
+{
+	int err;
+	unsigned char old_value;
+	unsigned char buf[2];
+
+	err = em3027_read_register(client, addr, &old_value, 1);
+	if (err)
+		return err;
+
+	buf[0] = addr;
+	buf[1] = (old_value & ~mask) | (value & mask);
+
+	return em3027_write_register(client, buf, ARRAY_SIZE(buf));
 }
 
 /**
@@ -160,10 +199,113 @@ static int em3027_set_time(struct device *dev, struct rtc_time *tm)
 	return em3027_write_register(client, buf, 8);
 }
 
+static int em3027_read_alarm(struct device *dev, struct rtc_wkalrm *wa)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	unsigned char buf[7];
+	int err;
+
+	err = em3027_read_register(client, EM3027_REG_ALARM_SEC, buf, 7);
+	if (err)
+		return err;
+
+	wa->time.tm_sec	 = bcd2bin(0x7f & buf[0]);
+	wa->time.tm_min	 = bcd2bin(0x7f & buf[1]);
+	wa->time.tm_hour = bcd2bin(0x7f & buf[2]);
+	wa->time.tm_mday = bcd2bin(0x7f & buf[3]);
+	wa->time.tm_wday = bcd2bin(0x7f & buf[4]);
+	wa->time.tm_mon	 = bcd2bin(0x7f & buf[5]);
+	wa->time.tm_year = bcd2bin(0x7f & buf[6]) + 80;
+
+	err = em3027_read_register(client, EM3027_REG_IRQ_CTRL, buf, 2);
+	if (err)
+		return -EIO;
+
+	wa->enabled = !!(buf[0] & EM3027_BIT_IRQ_ALARM);
+	wa->pending = (buf[1] & EM3027_BIT_IRQ_ALARM) && wa->enabled;
+
+	return 0;
+}
+
+static int em3027_set_alarm(struct device *dev, struct rtc_wkalrm *wa)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+	unsigned char buf[8];
+	int err;
+
+	buf[0] = EM3027_REG_ALARM_SEC;
+	buf[1] = (wa->time.tm_sec > 0)	? 0x80 | bin2bcd(wa->time.tm_sec)  : 0;
+	buf[2] = (wa->time.tm_min > 0)	? 0x80 | bin2bcd(wa->time.tm_min)  : 0;
+	buf[3] = (wa->time.tm_hour > 0)	? 0x80 | bin2bcd(wa->time.tm_hour) : 0;
+	buf[4] = (wa->time.tm_mday > 0)	? 0x80 | bin2bcd(wa->time.tm_mday) : 0;
+	buf[5] = (wa->time.tm_wday > 0)	? 0x80 | bin2bcd(wa->time.tm_wday) : 0;
+	buf[6] = (wa->time.tm_mon > 0)	? 0x80 | bin2bcd(wa->time.tm_mon)  : 0;
+	buf[7] = (wa->time.tm_year > 0)	?
+				0x80 | (bin2bcd(wa->time.tm_year) & 0x7f) : 0;
+
+	err = em3027_write_register(client, buf, 8);
+	if (err)
+		return -EIO;
+
+	if (wa->enabled) {
+		err = em3027_update_register(client, EM3027_REG_IRQ_CTRL,
+					     EM3027_BIT_IRQ_ALARM,
+					     EM3027_BIT_IRQ_ALARM);
+		if (err)
+			return err;
+	}
+
+	return 0;
+}
+
+static int em3027_alarm_irq_enable(struct device *dev, unsigned int enabled)
+{
+	struct i2c_client *client = to_i2c_client(dev);
+
+	return em3027_update_register(client, EM3027_REG_IRQ_CTRL,
+				      EM3027_BIT_IRQ_ALARM,
+				      (enabled ? EM3027_BIT_IRQ_ALARM : 0));
+}
+
 static const struct rtc_class_ops em3027_rtc_ops = {
 	.read_time = em3027_get_time,
 	.set_time = em3027_set_time,
+	.read_alarm = em3027_read_alarm,
+	.set_alarm = em3027_set_alarm,
+	.alarm_irq_enable = em3027_alarm_irq_enable,
 };
+
+static irqreturn_t em3027_irq_worker(int irq, void *dev_id)
+{
+	struct i2c_client *client = (struct i2c_client *)dev_id;
+	struct rtc_device *rtc = i2c_get_clientdata(client);
+	unsigned char irq_flags;
+	int err;
+
+	mutex_lock(&rtc->ops_lock);
+	err = em3027_read_register(client, EM3027_REG_IRQ_FLAGS, &irq_flags, 1);
+	if (err)
+		goto done;
+
+	if (irq_flags & EM3027_BIT_IRQ_ALARM) {
+		/* notify top layers */
+		rtc_update_irq(rtc, 1, RTC_IRQF | RTC_AF);
+		irq_flags &= ~EM3027_BIT_IRQ_ALARM;
+	}
+
+	if (irq_flags) {
+		dev_dbg(&client->dev,
+			"ignored IRQ(s) mask: %02x \n", irq_flags);
+		irq_flags = 0;
+	}
+
+	em3027_update_register(client, EM3027_REG_IRQ_FLAGS, EM3027_IRQ_MASK,
+			       irq_flags);
+
+done:
+	mutex_unlock(&rtc->ops_lock);
+	return IRQ_HANDLED;
+}
 
 static int em3027_probe(struct i2c_client *client,
 			const struct i2c_device_id *id)
@@ -192,6 +334,29 @@ static int em3027_probe(struct i2c_client *client,
 			goto fail;
 	}
 
+	if (client->irq > 0) {
+		/* configure rtc output as irq */
+		err = em3027_update_register(client, EM3027_REG_ON_OFF_CTRL,
+					     EM3027_BIT_ON_OFF_CLK_nINT, 0);
+		if (err)
+			goto fail;
+
+		/* clear pending interrupts */
+		err = em3027_update_register(client, EM3027_REG_IRQ_FLAGS,
+					     EM3027_IRQ_MASK, 0);
+		if (err)
+			goto fail;
+
+		err = request_threaded_irq(client->irq, NULL, em3027_irq_worker,
+					   IRQF_TRIGGER_LOW | IRQF_ONESHOT,
+					   em3027_driver.driver.name, client);
+		if (err != 0) {
+			dev_err(&client->dev, "%s: could not request irq %d \n",
+				__func__, client->irq);
+			goto fail;
+		}
+	}
+
 	return 0;
 
 fail:
@@ -204,8 +369,13 @@ static int em3027_remove(struct i2c_client *client)
 {
 	struct rtc_device *rtc = i2c_get_clientdata(client);
 
-	if (rtc)
-		rtc_device_unregister(rtc);
+	if (client->irq > 0) {
+		em3027_update_register(client, EM3027_REG_IRQ_CTRL,
+				       EM3027_BIT_IRQ_ALARM, 0);
+		free_irq(client->irq, client);
+	}
+
+	rtc_device_unregister(rtc);
 
 	return 0;
 }
